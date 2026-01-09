7be0de3492aeeb9e0982aa99c0a95cd78b6a3f76
Allan Alexandre <ursulinoallan@gmail.com>
2026-01-09 17:53:33 -0300

    Corrigido @shared/index.js e adicionado uuid

--- @shared/index.js
+++ @shared/index.js
@@ -1,7 +1,18 @@
-// @shared/index.js
+// @shared/index.js - VERSÃO CORRIGIDA
 const db = require('../database');
-const { formatPrice } = require('./format');
 
+// Função de formatação
+function formatPrice(amount, currency = 'BRL') {
+    const num = parseFloat(amount) || 0;
+    if (currency === 'BRL') {
+        return `R$ ${num.toFixed(2).replace('.', ',')}`;
+    } else if (currency === 'USD') {
+        return `$${num.toFixed(2)}`;
+    }
+    return `${num} ${currency}`;
+}
+
+// Exportar
 module.exports = {
     db,
     formatPrice
